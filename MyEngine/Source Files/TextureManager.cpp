c997bcdb9155516cbaa040bfc60516658a3c8b77
Priyanshu Kaushik <priyanshukaushik21@gmail.com>
2025-12-09 03:56:37 +0100

    texture bug fix

--- MyEngine/Source Files/TextureManager.cpp
+++ MyEngine/Source Files/TextureManager.cpp
@@ -55,9 +55,13 @@ uint32_t TextureManager::LoadTexture(const char *path){
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     
+    GLenum format = GL_RGB;
+    if (Channels == 4)
+        format = GL_RGBA;
+    
     if (data)
     {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, Texture.Width, Texture.Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, Texture.Width, Texture.Height, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     else
